5700f9612f937370f3a7ad7a39cb0dcc8a2bf25c
a_cherkasov <a_cherkasov@wargaming.net>
2024-06-19 10:43:37 +0200

    Transformations: glm included

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,13 +1,17 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 #include <iostream>
 #include <cmath>
 
 #include "Shader.hpp"
-
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb/stb_image.h"
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
